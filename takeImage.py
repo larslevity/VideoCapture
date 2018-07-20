626f17d990b84bfe3c4e735873455a58431ebbd2
bianca <bianca@bianca.amp>
2018-07-20 15:00:17 +0200

    maximal resolution of the image

--- takeImage.py
+++ takeImage.py
@@ -4,7 +4,7 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (2048, 2048)
+    camera.resolution = (2592, 1944)
     camera.start_preview()
     # Camera warm-up time
     time.sleep(1)
